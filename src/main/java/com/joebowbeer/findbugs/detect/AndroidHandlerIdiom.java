80755067a44b29aed096c4455c074298885a3b84
Joe Bowbeer <joe.bowbeer@gmail.com>
2011-10-28 00:33:48 -0700

    Churn.

--- src/main/java/com/joebowbeer/findbugs/detect/AndroidHandlerIdiom.java
+++ src/main/java/com/joebowbeer/findbugs/detect/AndroidHandlerIdiom.java
@@ -1,7 +1,6 @@
 package com.joebowbeer.findbugs.detect;
 
 import edu.umd.cs.findbugs.BugInstance;
-import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.ba.ch.Subtypes2;
 import edu.umd.cs.findbugs.bcel.PreorderDetector;
@@ -18,13 +17,8 @@ public class AndroidHandlerIdiom extends PreorderDetector {
     }
 
     @Override
-    public void visitClassContext(ClassContext classContext) {
-        classContext.getJavaClass().accept(this);
-    }
-
-    @Override
-    public void visit(JavaClass jclass) {
-        if (Subtypes2.instanceOf(jclass, "android.os.Handler") && hasOuterInstance(jclass)) {
+    public void visit(JavaClass obj) {
+        if (Subtypes2.instanceOf(obj, "android.os.Handler") && hasOuterInstance(obj)) {
             bugReporter.reportBug(
                     new BugInstance(this, "ANDROID_HANDLER_IDIOM", NORMAL_PRIORITY).addClass(this));
         }
